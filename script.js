5e1d5a07d2c40ca09005a011b4f57eb0b4ca0dee
akirio-create <marvelouscinderace@gmail.com>
2026-02-08 11:37:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const door1 = document.getElementById("d1");
 const door2 = document.getElementById("d2");
+const circle = document.querySelector(".circle");
 const heart = document.querySelector(".heart");
-const loaderIntro = document.querySelector(".loader-intro");
 
-const hLoop = gsap.from(heart, {
+const cLoop = gsap.from(circle, {
     opacity: 0.5,
     scale: 0.8,
     repeat: -1,
@@ -12,18 +12,18 @@ const hLoop = gsap.from(heart, {
     duration: 2,
 })
 
-heart.onclick = function(){
-    hLoop.pause();
+circle.onclick = function(){
+    cLoop.pause();
 
     const speed = 0.8/2;
-    const currentS = gsap.getProperty(heart, "scale");
+    const currentS = gsap.getProperty(circle, "scale");
     const targetS = 0.8;
     const difference = Math.abs(targetS - currentS);
     const dynamicTime = difference/speed;
 
     const tl = gsap.timeline();
 
-    tl.to(heart, {
+    tl.to(circle, {
         duration: dynamicTime,
         scale: 0.8,
         ease: "none"
@@ -38,26 +38,38 @@ heart.onclick = function(){
         ease: "power2.in",
         duration: 2.5
     }, "<")
-    tl.to(heart, {
-        scale: 0.7,
-        duration: 0.3,
-        ease: "power2.inOut"
+    tl.to(circle, {
+        scale: 0,
+        opacity: 0,
+        duration: 2.5,
+        ease: "power2.out"
     })
-    tl.to(loaderIntro, {
-        strokeDashoffset: 0, 
-        duration: 3,
-        ease: "none"
+    tl.to(heart, {
+        strokeDashoffset: 0,
+        duration: 1.5
+    }, "-=0.5")
+    tl.to(heart, {
+        fillOpacity: 1,
+        duration: 2,
+        
+    })
+    tl.to(heart, {
+        scale: 0.9,
+        duration: 1
+    })
+    tl.to(heart, {
+        scale: 1,
+        duration: 1,
+        stroke: "#FFAEC0",
+        filter: "drop-shadow(0 0 10px #FFAEC0)"
+    })
+    tl.to(heart, {
+        scale: 0.9,
+        duration: 1
     })
     tl.to(heart, {
         scale: 2,
         opacity: 0,
-        duration: 1.5,
-        ease: "power2.out"
+        duration: 0.5
     })
-    tl.to(loaderIntro, {
-        scale: 2,
-        opacity: 0,
-        duration: 1.5,
-        ease: "power2.out"
-    }, "<")
 }
